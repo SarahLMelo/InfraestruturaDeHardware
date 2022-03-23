63d074f55eae9cf96d015f38c03575c31ca6b727
SarahLMelo <61260788+SarahLMelo@users.noreply.github.com>
2022-03-23 13:49:36 -0300

    Acendendo os leds

--- Lista2/8.asm
+++ Lista2/8.asm
@@ -1,55 +1,45 @@
 loop:
-    jal x14, zera
+	add x13, x0, x0
+  	sb x12, 1033(x0)
+    sb x13, 1034(x0)
 
-    add x11, x0, x0
-    lb x10, 0(x0)
-    sb x10, 1030(x0)
-	lh x10, 1031(x0)
-
-    sb x12, 1033(x0)
-	jal x14, verde1
+	addi x13, x0, 0xfffff
+   	jal x14, verde1
 
 amarelo1:
-    addi x13, x0, 3
+	addi x12, x0, 6
+	sb x12, 1033(x0)
     sb x13, 1034(x0)
-	addi x11, x11, 1
     jal x14, end
 
 amarelo2:
-    addi x13, x0, 5
+	addi x12, x0, 9
+	sb x12, 1033(x0)
     sb x13, 1034(x0)
-	addi x11, x11, 1
     jal x14, end
-
 verde1:
-    addi x13, x0, 6
+    addi x12, x0, 10
+	sb x12, 1033(x0)
     sb x13, 1034(x0)
-	addi x11, x11, 1
     jal x14, end
 
 verde2:
-    addi x13, x0, 9
+    addi x12, x0, 11
+	sb x12, 1033(x0)
     sb x13, 1034(x0)
-	addi x11, x11, 1
     jal x14, end
 
 vermelho1:
-    addi x13, x0, 10
+    addi x12, x0, 3
+	sb x12, 1033(x0)
     sb x13, 1034(x0)
-	addi x11, x11, 1
     jal x14, end
 
 vermelho2:
-    addi x13, x0, 11
-    sb x13, 1034(x0)
-	addi x11, x11, 1
-
-zera:
-    sb x10, 1033(x0)
+    addi x12, x0, 5
+	sb x12, 1033(x0)
     sb x13, 1034(x0)
     jal x14, end
-
 end:
 
-	jal x11, loop
-
+	jal x14, loop
